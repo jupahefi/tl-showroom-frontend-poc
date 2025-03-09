faaf948bfe1f3461776500e873f097fd7d65db36
Juan Pablo Herrera Figueroa <jupahefi@gmail.com>
2025-03-09 11:53:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        strictPort: true,
-        proxy: {
-            "/api": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-                secure: false
-            }
-        }
+        strictPort: true
     },
     build: {
         outDir: "dist"
